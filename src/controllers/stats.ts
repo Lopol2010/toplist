dc608896271a7f307b320cde597ec91ae018f5e7
Evgeniy <evgeniy7@gmail.com>
2021-07-23 18:01:53 +0300

    set body color to dark

--- src/controllers/stats.ts
+++ src/controllers/stats.ts
@@ -23,6 +23,11 @@ export default class StatsController {
       { name: 'Low Gravity', id: 9 },
     ]
     let view = await ctx.render('top', { data: data })
+    let delay = await (new Promise((res, rej) => {
+      setTimeout(() => {
+        res(1)
+      }, 1000);
+    }))
     return view
   }
 

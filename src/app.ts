0ab0bc02d20a58f031e5db8c16f80fd8f6740dee
Evgeniy <evgeniy7@gmail.com>
2021-07-23 18:49:19 +0300

    add caching for static files (bg image and style.css for example)

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import * as cors from '@koa/cors'
 import * as Router from 'koa-router'
 import StatsController from './controllers/stats'
 
+const etag = require('koa-etag');
 var serve = require('koa-static')
 var ECT = require('ect')
 var renderer = ECT({ root : __dirname + '/views', ext : '.ect', watch: true })
@@ -26,8 +27,9 @@ export const app = new Koa()
     })
     app.use(cors({ origin: '*' }))
     app.use(bodyParser())
-    app.use(serve(__dirname + '/../static'))
-    app.use(serve(__dirname + '/views'))
+    app.use(etag());
+    app.use(serve(__dirname + '/../static', {maxage: 7 * 24 * 60 * 60 * 1000 }))
+    app.use(serve(__dirname + '/views', {maxage: 7 * 24 * 60 * 60 * 1000}))
     app.use(async (ctx, next) => { 
       ctx.render = renderer.render 
       return next()
